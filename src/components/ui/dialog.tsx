80bbd86efad3f66bb08244e4b79bca66b339ca39
Tipingouin17 <83900932+Tipingouin17@users.noreply.github.com>
2026-04-03 12:33:10 -0400

    fix: fix TypeScript null type for DialogPortal container prop

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -32,7 +32,7 @@ const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
 >(({ className, children, ...props }, ref) => (
-  <DialogPortal container={document.getElementById('root')}>
+  <DialogPortal container={document.getElementById('root') ?? undefined}>
     <DialogOverlay />
     <DialogPrimitive.Content
       ref={ref}
